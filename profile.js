dc07dd1cc28221c8a2e237f34afe20ebd652cc7d
Arstan Ash <arstanash@Arstans-MacBook-Pro.local>
2020-12-16 14:00:25 +0900

    signout

--- profile.js
+++ profile.js
@@ -2,6 +2,7 @@ var add = document.getElementById("add")
 var closebtn = document.getElementById("closebtn")
 var darken = document.getElementById("darken")
 var popup= document.getElementById("popup")
+var singout = document.getElementById("signout")
 add.addEventListener("click", function(){
     darken.classList.toggle("d-none")
     popup.classList.toggle("d-none")
@@ -10,3 +11,7 @@ closebtn.addEventListener("click", function (){
     popup.classList.toggle("d-none")
     darken.classList.toggle("d-none")
 })
+function signout(){
+    darken.classList.toggle("d-none")
+    singout.classList.toggle("d-none")
+}
