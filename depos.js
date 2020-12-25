f03b92fc686ca3b95d6dd052d28f560cde4ab315
Arstan Ash <arstanash@Arstans-MacBook-Pro.local>
2020-12-25 12:24:54 +0900

    depospopup

--- depos.js
+++ depos.js
@@ -1,8 +1,12 @@
+var ale = document.getElementById("ale")
 function copyToClipboard(element) {
     var $temp = $("<input>");
     $("body").append($temp);
     $temp.val($(element).text()).select();
     document.execCommand("copy");
     $temp.remove();
+    ale.classList.toggle("visible");
   }
-  
+function hide(){
+  ale.classList.toggle("visible")
+}
